7de760d101f896553e951fb4aabe0d17480f3d1b
velocity-cli <helper+cli@velocity.tech>
2021-03-02 11:27:11 +0200

    Brew formula update for mprod version v0.0.1-rc17

--- mprod.rb
+++ mprod.rb
@@ -5,16 +5,16 @@
 class Mprod < Formula
   desc ""
   homepage ""
-  version "0.0.1-rc16"
+  version "0.0.1-rc17"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://releases.velocity.tech/mprod/v0.0.1-rc16/mprod_0.0.1-rc16_Darwin_x86_64.tar.gz"
-    sha256 "e43a5baf9d4d36a5286aefa70a026511536ce6dc4997342e6794c4cc3192d0ba"
+    url "https://releases.velocity.tech/mprod/v0.0.1-rc17/mprod_0.0.1-rc17_Darwin_x86_64.tar.gz"
+    sha256 "740ff2e76e0eaa69137f6b6b6aede6eb187d51e1e81f225296eb9dbf0b1f51b5"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.velocity.tech/mprod/v0.0.1-rc16/mprod_0.0.1-rc16_Linux_x86_64.tar.gz"
-    sha256 "1ca7be074f97db6b73ca5378294414125743369381f94f619793bfde04eff0a8"
+    url "https://releases.velocity.tech/mprod/v0.0.1-rc17/mprod_0.0.1-rc17_Linux_x86_64.tar.gz"
+    sha256 "340c2c31f64933ff7a52214994c7bb67fa78ed6ff13410a6c6de4e5a0707ee49"
   end
 
   def install
